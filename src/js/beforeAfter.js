2980b57506e3ab2ee07e075ad06918a85d318ffa
Lola <yakjkf@gmail.com>
2020-06-05 22:55:41 +0600

    works portfolio modal

--- src/js/beforeAfter.js
+++ src/js/beforeAfter.js
@@ -3,31 +3,18 @@
     const container = document.querySelector('.before-after-container');
     const control = container.querySelector('.controls');
     const imageAfter = container.querySelector('.imageAfter');
+    let shiftX;
+    const md = new MobileDetect(window.navigator.userAgent);
+    const isMobile = md.mobile();
 
     control.onmousedown = function(event) {
         event.preventDefault(); // предотвратить запуск выделения (действие браузера)
 
-        let shiftX = event.clientX - control.getBoundingClientRect().left;
+        shiftX = event.clientX - control.getBoundingClientRect().left;
         // shiftY здесь не нужен, слайдер двигается только по горизонтали
 
         document.addEventListener('mousemove', onMouseMove);
         document.addEventListener('mouseup', onMouseUp);
-
-        function onMouseMove(event) {
-            let newLeft = event.clientX - shiftX - container.getBoundingClientRect().left;
-
-            // курсор вышел из слайдера => оставить бегунок в его границах.
-            if (newLeft < -(control.offsetWidth/2)) {
-                newLeft = -(control.offsetWidth/2);
-            }
-            let rightEdge = container.offsetWidth - control.offsetWidth/2;
-            if (newLeft > rightEdge) {
-                newLeft = rightEdge;
-            }
-
-            control.style.left = newLeft + 'px';
-            imageAfter.style.width = `${newLeft + 24}px`;
-        }
 
         function onMouseUp() {
             document.removeEventListener('mouseup', onMouseUp);
@@ -36,7 +23,43 @@
 
     };
 
+    function onMouseMove(event) {
+        let newLeft = event.clientX - shiftX - container.getBoundingClientRect().left;
+
+        // курсор вышел из слайдера => оставить бегунок в его границах.
+        if (newLeft < -(control.offsetWidth/2)) {
+            newLeft = -(control.offsetWidth/2);
+        }
+        let rightEdge = container.offsetWidth - control.offsetWidth/2;
+        if (newLeft > rightEdge) {
+            newLeft = rightEdge;
+        }
+
+        control.style.left = newLeft + 'px';
+        imageAfter.style.width = `${newLeft + 24}px`;
+    }
+
     control.ondragstart = function() {
         return false;
     };
+
+    console.log(isMobile);
+    if (isMobile) {
+        control.ontouchstart = function(event) {
+            event.preventDefault(); // предотвратить запуск выделения (действие браузера)
+
+            shiftX = event.clientX - control.getBoundingClientRect().left;
+            // shiftY здесь не нужен, слайдер двигается только по горизонтали
+
+            document.addEventListener('touchmove', onMouseMove);
+            document.addEventListener('touchend', onMouseUp);
+
+            function onMouseUp() {
+                document.removeEventListener('touchend', onMouseUp);
+                document.removeEventListener('touchmove', onMouseMove);
+            }
+
+        };
+    }
+
 })();
--- src/js/works.js
+++ src/js/works.js
@@ -3,12 +3,14 @@
     const portfolio = document.querySelector('.works');
     const works = portfolio.querySelectorAll('.image-wrapper');
     const hoverTemplate = document.getElementById('workHoverTemplate').innerHTML;
-    const portfolioTemplate = document.getElementById('portfolio01').innerHTML;
+
     const overlay = document.querySelector('.overlay');
 
     works.forEach(work =>{
+        //console.log(work);
         const photos = work.dataset.photos;
-        const hoverBlock = createHoverBlock(photos);
+        const portfolioNum = work.dataset.portfolio;
+        const hoverBlock = createHoverBlock(photos, portfolioNum);
         work.addEventListener('mouseenter', e => {
             work.insertAdjacentElement('beforeend', hoverBlock);
             setTimeout(() => hoverBlock.classList.add('active'), 50);
@@ -21,7 +23,7 @@
         });
     });
 
-    function createHoverBlock(photos) {
+    function createHoverBlock(photos, portfolioNum) {
         const wrapper = document.createElement('div');
         wrapper.className = 'work-hover-block';
         wrapper.innerHTML = hoverTemplate;
@@ -29,7 +31,8 @@
         photosSpan.innerHTML = photos;
         const btn = wrapper.querySelector('.btn');
         btn.addEventListener('click', e => {
-            console.log('Смотреть работу');
+            console.log('Смотреть работу - ', `portfolio${portfolioNum}`);
+            const portfolioTemplate = document.getElementById(`portfolio${portfolioNum}`).innerHTML;
             overlay.innerHTML = portfolioTemplate;
 
             overlay.style.display = 'block';
@@ -42,12 +45,4 @@
         return wrapper;
     }
 
-    const portfolioJSON = {
-        "project-name": {
-            "bedroom": 20,
-            "livingroom": 5,
-            "test": 1
-        }
-    };
-    console.log(portfolioJSON);
 })();
